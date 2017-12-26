d1bab78546a848fdd84f0c37927c861ebccd20c6
likj <mr.leezg@gmail.com>
2017-12-26 19:47:46 +0800

    测试分支

---
+++ src/main/java/cn/likj/second/App.java
@@ -0,0 +1,13 @@
+package cn.likj.second;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
